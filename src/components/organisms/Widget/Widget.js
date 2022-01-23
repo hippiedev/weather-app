678261b5904bb1cfaf7cd0b1dfeda885d94818f1
Jonathan Agusa <54756572+hippiedev@users.noreply.github.com>
2022-01-23 17:44:35 +0100

    Add env-example

--- src/components/organisms/Widget/Widget.js
+++ src/components/organisms/Widget/Widget.js
@@ -4,9 +4,13 @@ import styles from './Widget.module.css';
 const Widget = () => {
     return (
         <div className={styles.Widget}>
+        <div className={styles.CardWrap}>
             <Card/>
             <Card/>
             <Card/>
+            <Card/>
+            <Card/>
+        </div>
         </div>
     )
 }
